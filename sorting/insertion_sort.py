ff26284a1dd98a68eef368f3df832f1cad4c4bca
phanatagama <48324618+phanatagama@users.noreply.github.com>
2020-10-13 21:57:18 +0700

    Memperbaiki penulisan kode insertion sort
    Mengubah penamaan variabel dan fungsi menjadi snake_case dan memperbaiki indentasi/spasi

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,13 +1,14 @@
-def insertionSort(array):
-   for index in range(1,len(array)):
-     currentvalue = array[index]
-     position = index
-     while position>0 and array[position-1]>currentvalue:
-         array[position]=array[position-1]
-         position = position-1
-     array[position]=currentvalue
-   return array
+def insertion_sort(array):
+	for index_array in range(1, len(array)):
+		current_value = array[index_array]
+		position = index_array
+		while position > 0 and array[position-1] > current_value:
+			array[position] = array[position-1]
+			position = position-1
+		array[position] = current_value
+	return array
 
-array = [9,6,4,1,2,3,8]
-print(f"Array awal : {array}")
-print(f"Hasil insertion sort : {insertionSort(array)}")
+array_awal = [9, 6, 4, 1, 2, 3, 8]
+print(f"Array awal : {array_awal}")
+array_sort = insertion_sort(array_awal)
+print(f"Hasil insertion sort : {array_sort}")
